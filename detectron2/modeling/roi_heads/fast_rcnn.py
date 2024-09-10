eb3a1dea9f5e7b3bbc8b3e880f183a3ec66ab9d3
Desvita <59883177+Desvita@users.noreply.github.com>
2024-09-10 16:28:00 +0800

    Update fast_rcnn.py - weight 1.2

--- detectron2/modeling/roi_heads/fast_rcnn.py
+++ detectron2/modeling/roi_heads/fast_rcnn.py
@@ -346,7 +346,7 @@ class FastRCNNOutputLayers(nn.Module):
             brown_adipose_id = 3
             bone_cartilage_id = 1
             class_weights = torch.ones(scores.shape[1], device=scores.device)
-            class_weights = 2* class_weights
+            class_weights = 1.2* class_weights
             # class_weights[skeletal_muscle_id] = 2
             # class_weights[brown_adipose_id] = 2
             # class_weights[bone_cartilage_id] = 2
